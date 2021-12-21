f79ae5524e377efc5ed8ab372a630c02a7e3da3e
phil <flip.04@mail.ru>
2021-12-21 15:27:26 +0300

    lesson6 kts dependencies

---
+++ app/build.gradle.kts
@@ -0,0 +1,73 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-parcelize")
+    id("kotlin-kapt")
+}
+
+
+android {
+    compileSdk = 31
+
+    defaultConfig {
+        applicationId = "com.example.mydictionary"
+        minSdk = 21
+        targetSdk = 31
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(Dependencies.CORE_KTX_DEP)
+    implementation(Dependencies.APPCOMPAT_DEP)
+    implementation(Dependencies.MATERIAL_DEP)
+    implementation(Dependencies.CONSTRAINT_LAYOUT_DEP)
+    testImplementation(Dependencies.JUNIT_DEP)
+    androidTestImplementation(Dependencies.EXT_JUNIT_DEP)
+    androidTestImplementation(Dependencies.ESPRESSO_CORE_DEP)
+    implementation(Dependencies.CICERONE_DEP)
+    kapt(Dependencies.MOXY_COMPILER_DEP)
+    implementation(Dependencies.MOXY_KTX_DEP)
+    implementation(Dependencies.MOXY_ANDROIDX_DEP)
+    implementation(Dependencies.GLIDE_DEP)
+    annotationProcessor(Dependencies.GLIDE_COMPILER_DEP)
+    implementation(Dependencies.KOIN_CORE_DEP)
+    implementation(Dependencies.KOIN_ANDROID_COMPAT_DEP)
+    implementation(Dependencies.KOIN_ANDROID_DEP)
+    implementation(Dependencies.ROOM_RUNTIME_DEP)
+    kapt(Dependencies.ROOM_COMPILER_DEP)
+    implementation(Dependencies.ROOM_KTX_DEP)
+    implementation(Dependencies.OKHTTP_DEP)
+    implementation(Dependencies.RETROFIT_DEP)
+    implementation(Dependencies.RETROFIT_CONVERTER_GSON_DEP)
+    implementation(Dependencies.RETROFIT_ADAPTER_RXJAVA3_DEP)
+    implementation(Dependencies.RETROFIT_COROUTINES_ADAPTER_DEP)
+    implementation(Dependencies.CONSCRYPT_ANDROID_DEP)
+    implementation(Dependencies.COROUTINES_CORE_DEP)
+    implementation(Dependencies.COROUTINES_ANDROID_DEP)
+
+}
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,31 @@
+object Dependencies {
+    const val CORE_KTX_DEP = "androidx.core:core-ktx:${Versions.CORE_KTX_VERSION}"
+    const val APPCOMPAT_DEP = "androidx.appcompat:appcompat:${Versions.APPCOMPAT_VERSION}"
+    const val MATERIAL_DEP = "com.google.android.material:material:${Versions.MATERIAL_VERSION}"
+    const val CONSTRAINT_LAYOUT_DEP = "androidx.constraintlayout:constraintlayout:${Versions.CONSTRAINT_LAYOUT_VERSION}"
+    const val JUNIT_DEP = "junit:junit:${Versions.JUNIT_VERSION}"
+    const val EXT_JUNIT_DEP = ("androidx.test.ext:junit:${Versions.EXT_JUNIT_VERSION}")
+    const val ESPRESSO_CORE_DEP = ("androidx.test.espresso:espresso-core:${Versions.ESPRESSO_CORE_VERSION}")
+    const val CICERONE_DEP = ("com.github.terrakok:cicerone:${Versions.CICERONE_VERSION}")
+    const val MOXY_DEP = ("com.github.moxy-community:moxy:${Versions.MOXY_VERSION}")
+    const val MOXY_COMPILER_DEP = ("com.github.moxy-community:moxy-compiler:${Versions.MOXY_VERSION}")
+    const val MOXY_KTX_DEP = ("com.github.moxy-community:moxy-ktx:${Versions.MOXY_VERSION}")
+    const val MOXY_ANDROIDX_DEP = ("com.github.moxy-community:moxy-androidx:${Versions.MOXY_VERSION}")
+    const val ROOM_RUNTIME_DEP = ("androidx.room:room-runtime:${Versions.ROOM_VERSION}")
+    const val ROOM_COMPILER_DEP = ("androidx.room:room-compiler:${Versions.ROOM_VERSION}")
+    const val ROOM_KTX_DEP = ("androidx.room:room-ktx:${Versions.ROOM_VERSION}")
+    const val OKHTTP_DEP = ("com.squareup.okhttp3:okhttp:${Versions.OKHTTP_VERSION}")
+    const val RETROFIT_DEP = ("com.squareup.retrofit2:retrofit:${Versions.RETROFIT_VERSION}")
+    const val RETROFIT_CONVERTER_GSON_DEP = ("com.squareup.retrofit2:converter-gson:${Versions.RETROFIT_CONVERTER_GSON_VERSION}")
+    const val RETROFIT_ADAPTER_RXJAVA3_DEP = ("com.squareup.retrofit2:adapter-rxjava3:${Versions.RETROFIT_ADAPTER_RXJAVA3_VERSION}")
+    const val RETROFIT_COROUTINES_ADAPTER_DEP = ("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:${Versions.RETROFIT_COROUTINES_ADAPTER_VERSION}")
+    const val CONSCRYPT_ANDROID_DEP = ("org.conscrypt:conscrypt-android:${Versions.CONCRYPT_ANDROID_VERSION}")
+    const val GLIDE_DEP = ("com.github.bumptech.glide:glide:${Versions.GLIDE_VERSION}")
+    const val GLIDE_COMPILER_DEP = ("com.github.bumptech.glide:compiler:${Versions.GLIDE_VERSION}")
+    const val KOIN_CORE_DEP = ("io.insert-koin:koin-core:${Versions.KOIN_VERSION}")
+    const val KOIN_ANDROID_DEP = ("io.insert-koin:koin-android:${Versions.KOIN_VERSION}")
+    const val KOIN_ANDROID_COMPAT_DEP = ("io.insert-koin:koin-android-compat:${Versions.KOIN_VERSION}")
+    const val COROUTINES_CORE_DEP = ("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.COROUTINES_VERSION}")
+    const val COROUTINES_ANDROID_DEP = ("org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.COROUTINES_VERSION}")
+}
+
